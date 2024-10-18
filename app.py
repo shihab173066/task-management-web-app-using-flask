702c261b2cb93dfb235cc672152e5a280bf83c63
shihab173066 <shihab.hossainSH@gmail.com>
2024-10-18 16:34:09 +0600

    making the UI

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('dashboard/index.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('dashboard/about.html')
 
 if __name__== '__main__':
     app.run(debug=True)
